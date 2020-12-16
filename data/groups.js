2c5d00a9909a1754bb827203969a69979bad7d4b
cgiordano16 <cgiorda1@stevens.edu>
2020-12-16 12:12:20 -0500

    Group DB functions for adding, deleting, and inviting private and public members and admins.

--- data/groups.js
+++ data/groups.js
@@ -1,6 +1,7 @@
 const { ObjectID } = require('mongodb');
 const mongoCollections = require('../config/mongoCollections');
 const groups = mongoCollections.groups;
+const users = mongoCollections.users;
 const forumsModule = require('./forums');
 
 module.exports = {
@@ -12,13 +13,14 @@ module.exports = {
                 throw "ERROR: Name is already taken!"
             }
         }
-        if (!creator || !ObjectID(creator)) { throw `Error: there was an improper creator: ${creator} parameter when creating a group.` };
+        if (!creator || !ObjectID.isValid(creator)) { throw `Error: there was an improper creator: ${creator} parameter when creating a group.` };
         if (typeof private !== `boolean`) { throw `Error: the private parameter must be a boolean.` };
         if (!Array.isArray(groupTags)) { throw `Error: the groupTags paramter must be an array.` };
         let newGroup = {
             name: name,
             private: private,
             members: [creator],
+            owner: creator,
             adminsId: ObjectID(),
             admins: [creator],
             lists: [],
@@ -39,7 +41,7 @@ module.exports = {
         return recentGroup;
     },
     async read(id) {
-        if (!id || !ObjectID(id)) { throw `Error: the group parameter id for read() was not correct.` };
+        if (!id || !ObjectID.isValid(id)) { throw `Error: the groups parameter id for read() was not correct.` };
         const allGroups = await groups();
         try {
             let readGroup = await allGroups.findOne({ _id: id });
@@ -54,8 +56,14 @@ module.exports = {
         return groupList;
     },
 
+    async getAllPublic() {
+        const groupCollection = await groups();
+        const groupList = await groupCollection.find({ private: false }).toArray();
+        return groupList;
+    },
+
     async update(id, updatedBody) {
-        if (!id || !ObjectID(id)) { throw `Error: the thread parameter id for update() was not correct.` };
+        if (!id || !ObjectID.isValid(id)) { throw `Error: the groups parameter id for update() was not correct.` };
         const allGroups = await groups();
         const findGroup = await this.read(id);
         const updatedGroup = await allGroups.updateOne(
@@ -64,18 +72,220 @@ module.exports = {
         );
         return await this.read(id);
     },
-    async addAdmin(id) {
-        // TODO: Currently Working On has been a part of the delay
+    async addAdmin(groupId, userId, ownerId) {
+        if (!groupId || ObjectID.isValid(groupId)) { throw `Error: the group parameter groupId when adding an admin was incorrect.` }
+        if (!userId || ObjectID.isValid(userId)) { throw `Error: the group parameter userId when adding an admin was incorrect.` }
+        if (!ownerId || ObjectID.isValid(ownerId)) { throw `Error: the group parameter ownerId when adding an admin was incorrect.` }
+        try {
+            const allGroups = await groups();
+            let isMember = false;
+            const group = await this.read(groupId);
+            if (!group.owner.equals(ownerId)) {
+                throw `Error: Admin privledges cannot be given unless you are the owner.`;
+            }
+            for (let user of group.members) {
+                if (user.equals(userId)) {
+                    isMember = true;
+                    break;
+                }
+            }
+            if (isMember) {
+                for (let member of group.admins) {
+                    if (member.equals(userId)) {
+                        throw `Error: Member ${member} is already an admin in the group.`
+                    }
+                }
+                const addMember = await allGroups.updateOne(
+                    { _id: groupId },
+                    { $push: {admins: userId} }
+                );
+                return await this.read(groupId);
+            } else {
+                throw `This user was not a member of the group: ${groupId}`;
+            }
+        } catch (e) {
+            throw `Error when adding admin ${e}`;
+        }
 
     },
-    async addMember() {
-        // TODO: Currently Working On has been a part of the delay
+    async addPublicMember(groupId, userId) {
+        if (!groupId || ObjectID.isValid(groupId)) { throw `Error: the group parameter groupId when adding a public member was incorrect.` }
+        if (!userId || ObjectID.isValid(userId)) { throw `Error: the group parameter userId when adding a public member was incorrect.` }
+        try {
+            const allGroups = await groups();
+            const group = await this.read(groupId);
+            if (group.private) {
+                throw `Error: Group ${groupId} is private.`
+            }
+            for (let member of group.members) {
+                if (member.equals(userId)) {
+                    throw `Error: Member is already in the group.`
+                }
+            }
+            const addMember = await allGroups.updateOne(
+                { _id: groupId },
+                { $push: {members: userId} }
+            );
+            return await this.read(groupId);
+        } catch (e) {
+            throw `Error while adding public member: ${e}`
+        }
     },
-    async deleteAdmin() {
-        // TODO: Currently Working On has been a part of the delay
+
+    async invitePrivateMember(groupId, userId, adminId) {
+        if (!groupId || ObjectID.isValid(groupId)) { throw `Error: the group parameter groupId when inviting a user to a private group was incorrect.` }
+        if (!userId || ObjectID.isValid(userId)) { throw `Error: the group parameter userId when inviting a user to a private group was incorrect.` }
+        if (!ownerId || ObjectID.isValid(ownerId)) { throw `Error: the group parameter ownerId when inviting a user to a private group was incorrect.` }
+        try {
+            let isAdmin = false;
+            const allGroups = await groups();
+            const allUsers = await users();
+            const group = await this.read(groupId);
+            if (!group.private) {
+                throw `Error: Group ${groupId} is not a private group.`;
+            }
+            for (let admin of group.admins) {
+                if (admin.equals(adminId)) {
+                    isAdmin = true;
+                    break;
+                }
+            }
+            if (isAdmin) {
+                for (let member of group.members) {
+                    if (member.equals(userId)) {
+                        throw `Error: User ${userId} is already a member of group: ${groupId}`;
+                    }
+                }
+                for (let invitee of group.invitees) {
+                    if (invitee.equals(userId)) {
+                        throw `Error: User ${userId} is already an invitee of group: ${groupId}`;
+                    }
+                }
+                const addInvitee = await allGroups.updateOne(
+                    { _id: groupId },
+                    { $push: {invitees: userId} }
+                );
+                const sendInvite = await allUsers.updateOne(
+                    { _id: userId },
+                    { $push: {invitations: groupId} }
+                );
+                return await this.read(groupId);
+            }
+            
+        } catch (e) {
+            throw `Error while sending group invite: ${e}`;
+        }
     },
-    async deleteMember() {
-        // TODO: Currently Working On has been a part of the delay
+
+    async inviteResponse(groupId, userId, response = false) {
+        if (!groupId || ObjectID.isValid(groupId)) { throw `Error: the group parameter groupId when responding to invitations was incorrect.` }
+        if (!userId || ObjectID.isValid(userId)) { throw `Error: the group parameter userId when responding to invitations was incorrect.` }
+        try {
+            const allGroups = await groups();
+            const allUsers = await users();
+            const group = await this.read(groupId);
+            if (!group.private) {
+                throw `Error: Group ${groupId} is not a private group.`;
+            }
+            for (let invitee of group.invitees) {
+                if (invitee.equals(userId)) {
+                    if (response) {
+                        const acceptInvite = await allGroups.updateOne(
+                            { _id: groupId },
+                            { $push: {members: userId} }
+                        )
+                        const removeInvitee = await allGroups.updateOne(
+                            { _id: groupId },
+                            { $pull: {invitees: userId} }
+                        )
+                        const removeInvite = await allUsers.updateOne(
+                            { _id: userId },
+                            { $pull: {invitations: groupId} }
+                        )
+                        const addGroup = await allUsers.updateOne(
+                            { _id: userId},
+                            { $push: {groups: groupId} }
+                        )
+                        return await this.read(groupId);
+                    } else {
+                        const removeInvitee = await allGroups.updateOne(
+                            { _id: groupId },
+                            { $pull: {invitees: userId} }
+                        )
+                        const removeInvite = await allUsers.updateOne(
+                            { _id: userId },
+                            { $pull: {invitations: groupId} }
+                        )
+                        return await this.read(groupId);
+                    }
+                }
+            }
+        } catch (e) {
+            throw `Error while responding to invitation: ${e}`;
+        }
+    },
+
+    async deleteAdmin(groupId, adminId, ownerId) {
+        if (!groupId || ObjectID.isValid(groupId)) { throw `Error: the group parameter groupId when deleting an Admin was incorrect.` }
+        if (!adminId || ObjectID.isValid(adminId)) { throw `Error: the group parameter adminId when deleting an Admin was incorrect.` }
+        if (!ownerId || ObjectID.isValid(ownerId)) { throw `Error: the group parameter ownerId when deleting an Admin was incorrect.` }
+        try {
+            const allGroups = await groups();
+            const group = await this.read(groupId);
+            if (!group.owner.equals(ownerId)) {
+                throw `Error: Admin privledges cannot be removed unless you are the owner.`;
+            }
+            for (let admin of group.admins) {
+                if (admin.equals(adminId)) {
+                    const deleteAdmin = await allGroups.updateOne(
+                        { _id: groupId },
+                        { $pull: {admins: adminId} }
+                    );
+                    return await this.read(groupId);
+                }
+            }
+            throw `Error: User ${adminId} is not an admin of group: ${groupId}`;
+        } catch (e) {
+            throw `Error while deleting an admin: ${e}`;
+        }
+    },
+    async deleteMember(groupId, memberId, adminId) {
+        if (!groupId || ObjectID.isValid(groupId)) { throw `Error: the group parameter groupId when deleting a member was incorrect.` }
+        if (!adminId || ObjectID.isValid(adminId)) { throw `Error: the group parameter adminId when deleting a member was incorrect.` }
+        if (!ownerId || ObjectID.isValid(ownerId)) { throw `Error: the group parameter ownerId when deleting a member was incorrect.` }
+        try {
+            let isAdmin = false;
+            const allGroups = await groups();
+            const allUsers = await users();
+            const group = await this.read(groupId);
+            for (let admin of group.admins) {
+                if (admin.equals(adminId)) {
+                    isAdmin = true;
+                    break;
+                }
+            }
+            if (isAdmin) {
+                for (let member of group.members) {
+                    if (member.equals(memberId)) {
+                        const deleteMember = await allGroups.updateOne(
+                            { _id: groupId },
+                            { $pull: {members: memberId} }
+                        );
+                        const removeGroup = await allUsers.updateOne(
+                            { _id: memberId },
+                            { $pull: {groups: groupId} }
+                        )
+                        return await this.read(groupId);
+                    }
+                }
+                throw `Error: User ${memberId} is not a member of group: ${groupId}`;
+            } else {
+                throw `Error: User ${adminId} is not an admin of this group: ${groupId}`;
+            }
+
+        } catch (e) {
+            throw `Error while deleting an admin: ${e}`;
+        }
     },
     async delete(id) {
         if (!id || !ObjectID(id)) { throw `Error: the thread parameter id for delete() was not correct.` };
